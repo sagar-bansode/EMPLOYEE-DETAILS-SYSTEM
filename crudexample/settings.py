ef1e01132606c25af6cee630994837e660eced70
Sagar Bansode <sagarbansode2709@gmail.com>
2021-12-17 12:55:47 +0530

    first commit

--- crudexample/settings.py
+++ crudexample/settings.py
@@ -75,17 +75,23 @@ WSGI_APPLICATION = 'crudexample.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-     'default': {  
-        'ENGINE': 'django.db.backends.mysql',  
-        'NAME': 'djangodb',  
-        'USER':'root',  
-        'PASSWORD':'',  
-        'HOST':'localhost',  
-        'PORT':'3306'  
-    }  
-
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
+#DATABASES = {
+   #  'default': {  
+     #   'ENGINE': 'django.db.backends.mysql',  
+    #    'NAME': 'djangodb',  
+        #'USER':'root',  
+       # 'PASSWORD':'',  
+      #  'HOST':'localhost',  
+     #   'PORT':'3306'  
+    #}  
+
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
--- employee/admin.py
+++ employee/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import Employee
 # Register your models here.
-admin.site.register(Employee)
+
+
+class EmployeeAdmin(admin.ModelAdmin):
+ list_display = ('eid', 'ename','eemail','econtact')
+admin.site.register(Employee, EmployeeAdmin)
+
+
+#User name- sai
+#password- sai123
